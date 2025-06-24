912710c790a0828c0c03e1cae763b5d43e8edff7
AnaJuliaWeiss <anajuliaweiss035@gmail.com>
2025-06-24 13:40:58 -0300

    arrumando

--- NaturalMedicinaProjeto/naturalmedicina/controllers/PlantaController.php
+++ NaturalMedicinaProjeto/naturalmedicina/controllers/PlantaController.php
@@ -1,48 +1,34 @@
 <?php
-require_once __DIR__ . '/../config/database.php'; // Corrigido!
+require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../classes/PlantaClass.php';
 
 class PlantaController {
     private $conn;
+    private $planta;
 
     public function __construct() {
-        $this->conn = conectarBanco();
+        $database = new Database();
+        $this->conn = $database->getConnection();
+        $this->planta = new Planta($this->conn);
     }
 
     public function listarPlantas() {
-        $query = "SELECT * FROM plantas";
-        $stmt = $this->conn->prepare($query);
-        $stmt->execute();
-
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $this->planta->listarTodas();
     }
 
-    public function cadastrarPlanta($planta) {
-        $query = "INSERT INTO plantas (nome_popular, nome_cientifico, descricao, uso_medicinal, efeitos_colaterais, modo_uso, imagem_url, fonte)
-                  VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-        $stmt = $this->conn->prepare($query);
-
-        return $stmt->execute([
-            $planta->getNomePopular(),
-            $planta->getNomeCientifico(),
-            $planta->getDescricao(),
-            $planta->getUsoMedicinal(),
-            $planta->getEfeitosColaterais(),
-            $planta->getModoUso(),
-            $planta->getImagemUrl(),
-            $planta->getFonte()
-        ]);
+    public function buscarPlanta($id) {
+        return $this->planta->buscarPorId($id);
     }
 
-    public function buscarPlantaPorId($id) {
-        $query = "SELECT * FROM plantas WHERE id = ?";
-        $stmt = $this->conn->prepare($query);
-        $stmt->execute([$id]);
-        return $stmt->fetch(PDO::FETCH_ASSOC);
+    public function cadastrarPlanta($dados) {
+        return $this->planta->salvar($dados);
+    }
+
+    public function atualizarPlanta($id, $dados) {
+        return $this->planta->atualizar($id, $dados);
     }
 
     public function excluirPlanta($id) {
-        $query = "DELETE FROM plantas WHERE id = ?";
-        $stmt = $this->conn->prepare($query);
-        return $stmt->execute([$id]);
+        return $this->planta->excluir($id);
     }
+}
